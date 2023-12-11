17179ebaa9b1fa5456d0205979316b998885904e
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-11 16:23:13 -0500

    feat(AdventJS): a better solution for challenge 05

--- src/AdventJS/challenge05_again.ts
+++ src/AdventJS/challenge05_again.ts
@@ -1,29 +1,25 @@
-function cyberReindeer(road: string, time: number): string[] {
-  let template: string[] = Array.from(road);
-  const snapshots: string[] = [template.join("")];
-  template[0] = ".";
-  let santa_location: number = 0;
-
+function cyberReindeer(road: string, time: number) {
+  const snapshots = [road];
+  let template = "." + road.slice(1);
+  let santa_location = 0;
   for (let tick = 1; tick < time; tick++) {
     // Open barriers
     if (tick === 5) {
-      template = template.map((v) => (v === "|" ? "*" : v));
+      template = template.replace(/[|]/g, "*");
     }
 
     // Santa tries to move
-    if (
-      template[santa_location + 1] == "." ||
-      template[santa_location + 1] == "*"
-    ) {
+    if (template[santa_location + 1] !== "|") {
       santa_location += 1;
     }
 
     // Gets new state's string
-    const to_print = [...template];
-    to_print[santa_location] = "S";
-    snapshots.push(to_print.join(""));
+    const to_print =
+      template.slice(0, santa_location) +
+      "S" +
+      template.slice(santa_location + 1);
+    snapshots.push(to_print);
   }
-
   return snapshots;
 }
 
