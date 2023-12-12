3153f012d0cc4042cfe78b0c8ea7d1813adcc2a2
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-12 15:43:52 -0500

    feat(AdventJS): agregada plantilla reto 09

---
+++ src/AdventJS/challenge09.ts
@@ -0,0 +1,22 @@
+/*
+
+- Dos colores intercalados => ciclos de periodo 2
+- Pregunta cuantos cambios, no cuales => ¿se puede ignorar el valor la ultima luz?
+
+
+
+'🔴', '🔴', '🟢', '🟢', '🔴'
+'🔴', '🔴', '🟢', '🟢', '🟢'
+'🟢', '🟢', '🔴', '🔴', '🔴'
+'🟢', '🟢', '🔴', '🔴', '🟢'
+
+'🔴', '🟢', '🟢', '🟢', '🔴'
+'🔴', '🔴', '🟢', '🟢', '🟢'
+'🔴', '🔴', '🟢', '🟢', '🟢'
+
+*/
+
+function adjustLights(lights: string[]) {
+  // Cod
+  return 0;
+}
