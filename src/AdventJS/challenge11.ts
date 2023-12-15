30233d74fe76a11eafccaaad9b4ea8492c1bb660
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-15 15:24:09 -0500

    doc(AdventJS): planteada estrategía de solución

--- src/AdventJS/challenge11.ts
+++ src/AdventJS/challenge11.ts
@@ -15,9 +15,37 @@ Sí hay solución:
 - La posición de los no-match => el cambio involucra al menos una de las posiciones
 
 ¿el carácter con el cual cambiar debe ser equidistante a los dos no-match, y ser igual a alguno de los
-caracteres con missmatches 
+caracteres con mismatches?
+No. No es un punto equidistante, si no un punto tal que la distancia desde los mismatches es la misma
+
+Tratando de formalizar un poco...
+
+Sea 𝐬 una cadena de texto de longitud len(𝐬) con indices 0 ≤ 𝑖 < len(𝐬)
+
+(D1) La reflexión sobre los indices 𝑖 de 𝐬 se define como 𝐑(𝑖) = 𝑖' = len(𝐬)-1-𝑖
+(D1.1) 𝐬' = 𝐬[𝐑(len(𝐬)-1)], 𝐬[𝐑(len(𝐬)-2)], ..., 𝐬[𝐑(0)]
+(D1.2) Sí 𝐬[𝑖] = α ⟹ 𝐬[𝑖'] = α'
+
+(D2) La relación mismatches respecto a la reflexión 𝐔 = {(𝑖, 𝑖') | 𝐬[𝑖] ≠ 𝐬[𝑖']}, Dom(𝐔)={𝑖 | 0 ≤ 𝑖 < len(𝐬)}
+(D2.1) De la definición de 𝐔, se tiene que es reflexiva. (𝑖, 𝑖') ∈ 𝐔 ⟹ (𝑖', 𝑖) ∈ 𝐔
+(D3) Sea "pal" la propiedad ser palíndromo. pal(𝐬) ⟺ 𝐔 = ∅
+(D4) La pre imagen 𝐬⁻{α} = {𝑖 | 𝐬[𝑖] = α}
+(D4) La distancia 𝐃 esta dada por 𝐃(𝑖, 𝑖') = |𝑖 - 𝑗|
+
+---
+
+Hipótesis:
+
+Sí hay una ÚNICA solución.
+- n(𝐔) = 2, 𝐔 = {(𝑖, 𝑖'), (𝑖', 𝑖)} 
+- la solución {𝑚, 𝑛} satisface que (𝑚 = 𝑖 XOR 𝑚 = 𝑖'), (𝑛 ≠ 𝑖, 𝑖')
+- 𝐃(𝑖, 𝑛) = 𝐃(𝑛, 𝑖') > 0
+- Sí 𝑚 = 𝑖 ⟹ n ∈ 𝐬⁻{s[𝑖']} \ {𝑖'}
+
+  
 
 
+-----------------
 If it is not possible, null.
 If a palindrome can be formed with one change, an array with the two positions (indexes) that must be swapped to create it.
 If the palindrome can be formed with different swaps, always return the first one found.
