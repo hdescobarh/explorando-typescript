31ca3afa5c864c9fca9e8923dfafc4fed0096ed8
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-15 03:32:14 -0500

    feat(AdventJS): ahora genera un mapa de los match. Planteadas ideas de solución

--- src/AdventJS/challenge11.ts
+++ src/AdventJS/challenge11.ts
@@ -1,11 +1,46 @@
 /*
 
-If it is already a palindrome, an empty array.
+Sí alineamos cada palabra con su reversa tenemos un mapa de los no-match:
+
+(i) indice. (F) cadena derecha. (R) Cadena revés. (M) 1 sí match, 0 de lo contrario.
+
+i  0 1 2 3 4 5 6
+F  a a a b a b a
+R  a b a b a a a
+M  1 0 1 1 1 0 1
+
+Sí hay solución:
+- Par de no-match = a un cambio
+- Los caracteres en el no-match => caracteres involucrados en el cambio
+- La posición de los no-match => el cambio involucra al menos una de las posiciones
+
+¿el carácter con el cual cambiar debe ser equidistante a los dos no-match, y ser igual a alguno de los
+caracteres con missmatches 
+
+
 If it is not possible, null.
 If a palindrome can be formed with one change, an array with the two positions (indexes) that must be swapped to create it.
 If the palindrome can be formed with different swaps, always return the first one found.
 */
 function getIndexsForPalindrome(word: string) {
+  const forward = Array.from(word);
+  const get_matches = (forward: string[]) => {
+    const reverse_term = forward.length - 1;
+    let no_matches = 0;
+    const match_map: boolean[] = [];
+    for (let i = 0; i < forward.length; i++) {
+      const match = forward[i] === forward[reverse_term - i];
+      no_matches += match ? 0 : 1;
+      match_map.push(match);
+    }
+    return [no_matches, match_map];
+  };
+
+  //If it is already a palindrome, an empty array.
+  const [no_matches, match_map] = get_matches(forward);
+  if (no_matches === 0) {
+    return [];
+  }
   return [0, 0];
 }
 
@@ -20,9 +55,9 @@ const test_cases: [string, number[] | null][] = [
 
 for (const [word, expected] of test_cases) {
   const output = getIndexsForPalindrome(word);
-  if (output.toString() === expected?.toString()) {
+  if (output.toString() !== expected?.toString()) {
     console.log(
-      `Failed with: ${word}\nExpected: ${expected}. Obtained: ${output}`
+      `Failed with: ${word}\nExpected: ${expected}. Obtained: ${output}\n`
     );
   }
 }
